0c233022b88dbc0e732297cc349bf2ac3b9ce9a9
hansterhorst <hans.novi@icloud.com>
2022-01-04 22:11:09 +0100

    create LoginRegisterForm

---
+++ src/components/form-inputs/LoginRegisterForm.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import StyledButton from "../../styles/StyledButton";
+import StyledLink from "../../styles/StyledLink";
+
+
+export default function LoginRegisterForm({children, title, onSubmit, submitButtonTitle, orButtonTitle}) {
+
+   return (
+      <StyledForm onSubmit={onSubmit}>
+         <h1>{title}</h1>
+
+         {children}
+
+         <div className="form-footer">
+            <label>* Verplichte velden</label>
+            <StyledButton type="onsubmit">{submitButtonTitle}</StyledButton>
+         </div>
+
+         <div className="or-register">
+            <StyledLink to="/register">{orButtonTitle}</StyledLink>
+         </div>
+      </StyledForm>
+   )
+}
+
+
+const StyledForm = styled.form`
+
+  padding: 3rem 0;
+
+  h1 {
+    color: ${({theme: {colors}}) => colors.red};
+    margin: 2rem 0;
+  }
+
+  .form-footer {
+    display: flex;
+    justify-content: space-between;
+
+    label {
+      color: ${({theme: {colors}}) => colors.green};
+      font-size: 1.6rem;
+      font-weight: 500;
+    }
+  }
+
+  .or-register {
+    margin: 3rem 0;
+    text-align: center;
+  }
+
+`
