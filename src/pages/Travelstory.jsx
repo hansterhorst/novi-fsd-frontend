6905ae58811221621b2f429a980fe6e7c0be7926
hansterhorst <hans.novi@icloud.com>
2022-01-05 21:55:42 +0100

    refactor Travelstory page

--- src/pages/Travelstory.jsx
+++ src/pages/Travelstory.jsx
@@ -12,13 +12,20 @@ import ProfileImage from "../components/ProfileImage";
 import StyledLink from "../styles/StyledLink";
 import StyledHeader from "../styles/StyledHeader";
 import {pageNavLinks} from "./pageNavLinks";
+import TextArea from "../components/form-inputs/TextArea";
+import {useForm} from "react-hook-form";
 
 
 export default function Travelstory() {
 
-   const [formData, setFormData] = useState("")
    let [travelstory, setTravelstory] = useState([])
    let {id} = useParams()
+
+   const {register, handleSubmit} = useForm()
+
+   function onSubmit(data) {
+      console.log(data)
+   }
 
    const getTravelstoryById = async () => {
       try {
@@ -29,17 +36,6 @@ export default function Travelstory() {
       } catch (error) {
          console.error(error);
       }
-   }
-
-   function handleOnChange(e) {
-      setFormData({
-         [e.target.name]: e.target.value
-      })
-   }
-
-   function handleOnSubmit(e) {
-      e.preventDefault()
-      console.log(formData)
    }
 
 
@@ -53,25 +49,31 @@ export default function Travelstory() {
 
    return (
       <Layout navLinks={pageNavLinks.user}>
+
          <StyledHeader bgImage={imageUrl}/>
+
          <Container bgColor={({theme}) => theme.colors.darkGray}>
             <StyledArticleHeader>
-               <div className="profile-image">
-                  <ProfileImage/>
-                  <Link to={`/user/${userId}`}>
-                     <h3>{author}</h3>
-                  </Link>
-                  <StyledButton> ❤ 10 Likes</StyledButton>
-               </div>
+
                <div className="article-header-titles">
                   <h2>{country}</h2>
                   <h1>{title}</h1>
                   <h3>{tripType}</h3>
                   <h4>{dateToLocalString(tripDate)}</h4>
                </div>
+
+               <div className="article-header-profile">
+                  <ProfileImage squareSize={150}/>
+                  <Link to={`/user/${userId}`}>
+                     <h3>{author}</h3>
+                  </Link>
+                  <StyledButton> ❤ 10 Likes</StyledButton>
+               </div>
+
             </StyledArticleHeader>
          </Container>
-         <Container bgImage={whiteAltitudeLines} maxWidth={900}>
+
+         <Container bgImage={whiteAltitudeLines} maxWidth={800}>
             <StyledArticle>
                <p>{article}</p>
                <StyledLink to={`/travelstories`}>terug</StyledLink>
@@ -80,24 +82,21 @@ export default function Travelstory() {
 
          <Container bgImage={greenAltitudeLines} maxWidth={800}>
 
-            <StyledForm onSubmit={(e) => handleOnSubmit(e)}>
-               <h3>Please keep the conversation civil, constructive, and inclusive, or your comment
-                  will be removed.</h3>
-               <label>Comment
-                  <textarea name="comment" value={formData.comment}
-                            onChange={e => handleOnChange(e)} wrap="hard"/>
-               </label>
+            <StyledForm onSubmit={handleSubmit(onSubmit)}>
+               <h3>Houd uw reactie beschaafd, constructief en inclusief, anders wordt uw reactie
+                  verwijderd.</h3>
+
+               <TextArea labelTitle="Reactie" name="comment" register={register} height={100}/>
+
                <div className="form-footer">
                   <StyledButton type="onsubmit">Verstuur reactie</StyledButton>
                </div>
             </StyledForm>
 
-            <StyledComment>
-               <div className="profile-image">
-                  <ProfileImage/>
-               </div>
+            <StyledComments>
+               <ProfileImage/>
                <div className="comment">
-                  <div className="details">
+                  <div className="comment-profile">
                      <h3>Hans ter Horst </h3>
                      <h2>•</h2>
                      <p>12 december 2021</p>
@@ -106,7 +105,7 @@ export default function Travelstory() {
                      asfalt fietsen maar heb vandaag toch wel genoten. Morgen het IJsselmeer
                      over</p>
                </div>
-            </StyledComment>
+            </StyledComments>
          </Container>
       </Layout>
    )
@@ -116,9 +115,8 @@ const StyledArticleHeader = styled.div`
   position: relative;
   padding: 7rem 0;
 
-
   .article-header-titles {
-    width: calc(100% - 150px);
+    width: calc(100% - 200px);
 
     h1, h2, h3 {
       color: ${({theme}) => theme.colors.red};
@@ -129,31 +127,28 @@ const StyledArticleHeader = styled.div`
       color: ${({theme}) => theme.colors.green};
     }
 
-    h2 {
-      font-size: 2rem;
-    }
   }
 
-  .profile-image {
+  .article-header-profile {
     position: absolute;
     top: -70px;
     right: 0;
-    height: 150px;
-    width: 150px;
-    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     h3 {
       color: ${({theme: {colors}}) => colors.green};
-      font-size: 2rem;
-      font-weight: bold;
+      font-weight: 700;
       margin: 1rem 0 2rem;
+      width: 200px;
+      text-align: center;
     }
   }
 
 `
 
 const StyledArticle = styled.article`
-
   padding: 5rem 0;
 
   p {
@@ -163,7 +158,7 @@ const StyledArticle = styled.article`
 `
 
 const StyledForm = styled.form`
-  padding: 5rem 0;
+  padding: 5rem 0 0;
 
   h3 {
     color: ${({theme: {colors}}) => colors.red};
@@ -171,41 +166,17 @@ const StyledForm = styled.form`
     margin: 2rem 0;
   }
 
-  label {
-    color: ${({theme: {colors}}) => colors.white};
-    font-size: 1.6rem;
-  }
-
-
-  textarea {
-    padding: 0.8rem 1.6rem;
-    font-family: "Merriweather", serif;
-    font-size: 1.8rem;
-    color: ${({theme: {colors}}) => colors.black};
-    border: 3px solid ${({theme: {colors}}) => colors.green};
-    width: 100%;
-    background-color: ${({theme: {colors}}) => colors.white};
-    margin-bottom: 1.5rem;
-    display: block;
-    height: 100px;
-  }
-
   .form-footer {
     display: flex;
     justify-content: flex-end;
+    margin: 2rem 0;
   }
-
-
 `
 
-const StyledComment = styled.div`
+const StyledComments = styled.section`
   display: flex;
   align-items: flex-start;
-
-  .profile-image {
-    height: 100px;
-    width: 100px;
-  }
+  padding: 3rem 0;
 
   .comment {
     padding: 0 1rem;
@@ -215,7 +186,7 @@ const StyledComment = styled.div`
       font-size: 1.6rem;
     }
 
-    .details {
+    .comment-profile {
       display: flex;
       align-items: center;
       gap: 1rem;
