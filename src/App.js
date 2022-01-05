8d8f90491eacf236b56b841698471f9697da5bd5
hansterhorst <hans.novi@icloud.com>
2022-01-05 13:13:59 +0100

    add Travelstories page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import {Routes, Route} from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Travelstories from "./pages/Travelstories";
 
 function App() {
    return (
@@ -16,6 +17,7 @@ function App() {
                <Route path="/" element={<Home/>}/>
                <Route path="/register" element={<Register/>}/>
                <Route path="/login" element={<Login/>}/>
+               <Route path="/travelstories" element={<Travelstories/>}/>
             </Routes>
       </StyledThemesProvider>
    );
