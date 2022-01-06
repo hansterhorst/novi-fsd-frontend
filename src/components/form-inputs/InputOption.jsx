2cbf450fc48b87f70a1e9ea9d5a3327e66cc16f6
hansterhorst <hans.novi@icloud.com>
2022-01-06 20:09:46 +0100

    create InputOption component

---
+++ src/components/form-inputs/InputOption.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import StyledLabel from "../../styles/StyledLabel";
+
+export default function InputOption({
+                                       labelTitle,
+                                       name,
+                                       required = false,
+                                       register,
+                                       options,
+                                       placeholder
+                                    }) {
+
+   return (
+      <StyledLabel>{labelTitle}
+         <StyledSelect type="option" {...register(`${name}`)} required={required}>
+            <option value="Vakantie">{placeholder}</option>
+            {options.map((option, index) => (
+               <option key={index} value={option}>{option}</option>
+            ))}
+         </StyledSelect>
+      </StyledLabel>
+   )
+
+}
+
+const StyledSelect = styled.select`
+  padding: 0.8rem 1.6rem;
+  font-family: "Merriweather", serif;
+  font-size: 1.8rem;
+  color: ${({theme: {colors}}) => colors.black};
+  border: 3px solid ${({theme: {colors}}) => colors.green};
+  width: 100%;
+  background-color: ${({theme: {colors}}) => colors.white};
+  margin-bottom: 1.5rem;
+`
