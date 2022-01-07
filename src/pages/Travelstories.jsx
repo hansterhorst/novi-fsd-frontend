a8d4990b1d530c877d8046a6aa33ea9d77e88d05
hansterhorst <hans.novi@icloud.com>
2022-01-07 14:15:00 +0100

    refactor Travelstories page and add TravelstoriesGrid

--- src/pages/Travelstories.jsx
+++ src/pages/Travelstories.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import styled from "styled-components";
 import Layout from "../components/layout/Layout";
 import whiteAltitudeLines from "../assets/images/white-altitude-lines.png"
-import TravelstoryCard from "../components/TravelstoryCard";
 import Container from "../components/Container";
 import {pageNavLinks} from "./pageNavLinks";
 import {Link} from "react-router-dom";
+import TravelstoriesGrid from "../components/travelstory/TravelstoriesGrid";
 
 
 export default function Travelstories() {
@@ -19,10 +19,10 @@ export default function Travelstories() {
 
       try {
          const response = await axios.get('http://localhost:8080/api/v1/travelstories/');
-         const gridArray = threeColumnsGridArray(response.data)
-         setTravelstories(gridArray)
 
-         randomTravelstory(gridArray)
+         setTravelstories(response.data)
+
+         randomTravelstory(response.data)
 
       } catch (error) {
          console.error(error);
@@ -54,38 +54,12 @@ export default function Travelstories() {
    * METHODES
    * */
 
-   function threeColumnsGridArray(dataArray) {
-      let gridArray = []
-      let rowArray = []
-
-      // eslint-disable-next-line array-callback-return
-      dataArray.map((story, index) => {
-         rowArray.push(story)
-         if ((index + 1) % 3 === 0) {
-            gridArray.push(rowArray)
-            rowArray = []
-         }
-         if (index + 1 === dataArray.length) gridArray.push(rowArray)
-      })
-      return gridArray
-   }
-
-
    function randomTravelstory(array) {
-
-      let newArray = []
-
-      array.map(rows => {
-         return rows.map(story => {
-            return newArray.push(story)
-         })
-      })
-
-      const index = Math.floor(Math.random() * newArray.length)
-      setTravelstory(newArray[index])
+      const index = Math.floor(Math.random() * array.length)
+      setTravelstory(array[index])
    }
 
-   const {id,imageUrl, country, title, author} = travelstory
+   const {id, imageUrl, country, title, author} = travelstory
 
    return (
       <Layout navLinks={pageNavLinks.user}>
@@ -99,14 +73,9 @@ export default function Travelstories() {
             </StyledHeader>
          </Link>
          <Container bgImage={whiteAltitudeLines} maxWidth={1200}>
-            <h1>Travel Stories</h1>
-            {travelstories.map((rowsArray, index) => (
-               <StyledThreeColumnsGrid key={index} index={index}>
-                  {rowsArray.map(travelstory => (
-                     <TravelstoryCard key={travelstory.id} travelstory={travelstory}/>
-                  ))}
-               </StyledThreeColumnsGrid>
-            ))}
+
+            <TravelstoriesGrid dataArray={travelstories} title="Laatste TravelStories"/>
+
          </Container>
       </Layout>
    );
@@ -131,10 +100,5 @@ const StyledHeader = styled.header`
   h4 {
     font-size: 2rem;
   }
-`;
-
-const StyledThreeColumnsGrid = styled.main`
-  display: grid;
-  grid-template-columns: ${({index}) => index % 2 === 0 ? `2fr 1fr 1fr` : `1fr 1fr 2fr`};
-  grid-auto-flow: column;
 `
+
