83d9c275fdb580835cfb8b445a81fc8f08decc05
hansterhorst <hans.novi@icloud.com>
2022-01-08 13:21:17 +0100

    create InputPassword component

---
+++ src/components/form-inputs/InputPassword.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+import StyledLabel from "../../styles/StyledLabel";
+
+export default function InputPassword({
+                                         labelTitle,
+                                         name,
+                                         required = false,
+                                         register
+                                      }) {
+
+   const [show, setShow] = useState(false)
+
+   return (
+      <StyledPasswordLabel>{labelTitle}
+         <button onClick={() => setShow(!show)}>{show ? "hide" : "show"}</button>
+         <StyledInput type={show ? "text" : "password"} {...register(`${name}`)}
+                      required={required}/>
+      </StyledPasswordLabel>
+   )
+}
+
+const StyledPasswordLabel = styled(StyledLabel)`
+  button {
+    border: none;
+    color: ${({theme: {colors}}) => colors.green};
+    font-weight: normal;
+    font-size: 1.4rem;
+    margin: 0 auto;
+
+  }
+`
+
+const StyledInput = styled.input`
+  padding: 0.8rem 1.6rem;
+  font-family: "Merriweather", serif;
+  font-size: 1.8rem;
+  color: ${({theme: {colors}}) => colors.black};
+  border: 3px solid ${({theme: {colors}}) => colors.green};
+  width: 100%;
+  background-color: ${({theme: {colors}}) => colors.white};
+  margin-bottom: 1.5rem;
+  height: auto;
+`
