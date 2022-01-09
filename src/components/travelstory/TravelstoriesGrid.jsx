d240dbd381c6b25dea6fd739d17df363b94b7ffa
hansterhorst <hans.novi@icloud.com>
2022-01-09 13:28:58 +0100

    refactor TravelstoryCard file

--- src/components/travelstory/TravelstoriesGrid.jsx
+++ src/components/travelstory/TravelstoriesGrid.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import TravelstoryCard from "../TravelstoryCard";
+import TravelstoryCard from "./TravelstoryCard";
 import styled from "styled-components";
 
 
