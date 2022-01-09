b634eccc4e23703fe7374dd0a8a6a1b393ddae3f
hansterhorst <hans.novi@icloud.com>
2022-01-09 12:38:28 +0100

    add useNavigate to go back to the previous page

--- src/components/form-inputs/CreateEditForm.jsx
+++ src/components/form-inputs/CreateEditForm.jsx
@@ -6,8 +6,11 @@ import TextArea from "./TextArea";
 import StyledLabel from "../../styles/StyledLabel";
 import StyledButton from "../../styles/StyledButton";
 import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
 
 export default function CreateEditForm({onSubmit, register, submitButtonTitle}) {
+
+   const navigate = useNavigate()
 
    return (
       <StyledForm onSubmit={onSubmit}>
@@ -28,7 +31,7 @@ export default function CreateEditForm({onSubmit, register, submitButtonTitle}) 
 
          <InputField labelTitle="* Image link" name="imageUrl" register={register}/>
 
-         <InputCheckbox labelTitle="Mag iedereen uw reisverhaal lezen?" name="isPrivate"
+         <InputCheckbox labelTitle="Mag iedereen uw reisverhaal lezen?" name="isPublic"
                         checked={true} register={register}/>
 
          <TextArea labelTitle="* Reisverhaal" name="article" register={register}
@@ -37,6 +40,7 @@ export default function CreateEditForm({onSubmit, register, submitButtonTitle}) 
          <div className="form-footer">
             <StyledLabel>* Verplichte velden</StyledLabel>
             <div className="form-buttons">
+               <StyledButton onClick={() => navigate(-1)}>Cancel</StyledButton>
                <StyledButton type="onsubmit">{submitButtonTitle}</StyledButton>
             </div>
          </div>
@@ -60,8 +64,6 @@ const StyledForm = styled.form`
 
   .input-container {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
   }
 
 
@@ -69,6 +71,11 @@ const StyledForm = styled.form`
     display: flex;
     justify-content: space-between;
     margin-top: 2rem;
+
+    .form-buttons {
+      display: flex;
+      column-gap: 2rem;
+    }
 
     label {
       width: auto;
